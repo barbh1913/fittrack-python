9c862199d4465f7127457944fc1ae5772bce25bd
Bar Ben Hamo <barbh1913@gmail.com>
2026-01-19 18:04:01 +0200

    Clean project structure

--- PythonProject6 - Copy/backend/app/exceptions/__init__.py
+++
@@ -1,1 +0,0 @@
-# Custom exceptions + HTTP error mapping
--- PythonProject6 - Copy/start-dev.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "%~dp0"
-cd "frontend\static"
-echo Starting React development server...
-echo Make sure Flask is running on port 5000!
-echo.
-call npm run dev
